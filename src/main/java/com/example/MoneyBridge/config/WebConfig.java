433a7cf914015cf0a7663c7540fe0eec04dcbfa3
Sanjith S <sanjithsece@gmail.com>
2025-07-25 20:58:01 +0530

    changes

--- src/main/java/com/example/MoneyBridge/config/WebConfig.java
+++ src/main/java/com/example/MoneyBridge/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://money-bridge-five.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
